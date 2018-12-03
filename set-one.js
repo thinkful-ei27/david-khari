8aa876fce6658bf969ccd7a02ba5fd2465f59e83
kriviears <kriviears@gmail.com>
2018-12-03 17:17:22 -0500

    caught all the errors

--- set-one.js
+++ set-one.js
@@ -1,22 +1,31 @@
 function createGreeting(name, age) {
-    if (name !== undefined || age !== undefined) {
-        const yearOfBirth = getYearOfBirth(age);
-        return 'Hi, my name is Khari';
-    } else {
-        throw new Error("Arguments not valid")
-    }
-}
-
-function getYearOfBirth(age){
-    if (age < 0) {
-        throw new Error("Age can not be negative");
-    }
-    try {
-        const greeting1 = createGreating(age);
-    } catch {
-        console.error(Error.message);
-    }
-    return 2018 - age;
-}   
-const greeting1 = createGreeting("David", 20);
-console.log(greeting1);
+    if (typeof name !== "string" || typeof age !== "number"){
+        throw new TypeError('Name must be a string and age must be a number');
+      }
+      try{
+        
+      }
+      catch(e){
+        console.error(e.message);
+      }
+      if (name !== undefined || age !== undefined) {
+          const yearOfBirth = getYearOfBirth(age);
+          return 'Hi, my name is ' + name;
+      } else {
+          throw new Error("Arguments not valid")
+      }
+  }
+  
+  function getYearOfBirth(age){
+      if (age < 0) {
+          throw new Error('Age can not be negative');
+      }
+      try {
+      } catch(e) {
+          console.error(e.message);
+      }
+      return 2018 - age;
+  }   
+  const greeting1 = createGreeting("David", -25);
+  console.log(greeting1);
+  
