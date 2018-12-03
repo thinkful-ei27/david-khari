93b84995e6cce74ecd7f2ae15d18c3f8bc47aff3
kriviears <kriviears@gmail.com>
2018-12-03 17:54:59 -0500

    'beyond' solved with if statements... still wondering better expression of infinite in JS

--- set-2.js
+++ set-2.js
@@ -8,3 +8,18 @@ function jediName(firstName, lastName) {
 }
 
 console.log(jediName("David", "Johnson"));
+
+function beyond(num){
+    
+    if (num>1.797693134862315E+308 || num<-1.797693134862315E+308){
+        console.log('And Beyond');}
+    else if (num>0){
+        console.log('To infinity');}
+    else if (num<0){
+        console.log('To negative infinity');}
+    else if (num==0){
+        console.log('Staying home');}
+    }
+
+
+beyond(5);
