583ae8a89f54f397ad8afc6ecef29bb0ab898304
kriviears <kriviears@gmail.com>
2018-12-03 16:15:44 -0500

    getYearOfBirth calculated in separate function

--- set-one.js
+++ set-one.js
@@ -1,7 +1,10 @@
 function createGreeting(name, age) {
-    const yearOfBirth = 2018 - age;
+    const yearOfBirth = getYearOfBirth(age);
     return 'Hi, my name is Khari';
 }
 
+function getYearOfBirth(age){
+    return 2018 - age;
+}
 const greeting1 = createGreeting();
 console.log(greeting1);
