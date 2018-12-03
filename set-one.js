585888d7ee89a04dc2c5048482139e5ea961ccaa
Clack321 <dajohnson1998@gmail.com>
2018-12-03 16:28:59 -0500

    added a try catch block to check if age is greater than zero

--- set-one.js
+++ set-one.js
@@ -4,7 +4,15 @@ function createGreeting(name, age) {
 }
 
 function getYearOfBirth(age){
+    if (age < 0) {
+        throw new Error("Age can not be negative");
+    }
+    try {
+        const greeting1 = createGreating(age);
+    } catch {
+        console.error(Error.message);
+    }
     return 2018 - age;
-}
+}   
 const greeting1 = createGreeting();
 console.log(greeting1);
